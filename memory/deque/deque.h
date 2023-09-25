d1bfb845151a308f299f3f112c84fd5c6c5b0eba
Mark Bondarenko <kingsman2gold@gmail.com>
2023-09-25 11:11:41 +0200

    memory/deque

--- memory/deque/deque.h
+++ memory/deque/deque.h
@@ -5,62 +5,57 @@
 #include <deque>
 
 class Deque {
-private:
-    static const size_t kBufferSize = 128;
-
-    struct Buffer {
-        int* data;
-        bool have_data = false;
-
-        Buffer() = default;
-
-        void Alloc();
-
-        void Dealloc();
-
-        int& operator[](size_t index);
-
-        int operator[](size_t index) const;
-    };
-
 public:
     Deque() = default;
-    Deque(const Deque& other);
-    Deque(Deque&& other);
-    explicit Deque(size_t size);
+    Deque(const Deque& rhs) = default;
+    Deque(Deque&& rhs) = default;
+    explicit Deque(size_t size) : data_(size) {
+    }
 
-    Deque(std::initializer_list<int> list);
+    Deque(std::initializer_list<int> list) : data_(list) {
+    }
 
-    Deque& operator=(Deque other);
+    Deque& operator=(Deque rhs) {
+        Swap(rhs);
+        return *this;
+    }
 
-    void Swap(Deque& other);
+    void Swap(Deque& rhs) {
+        std::swap(data_, rhs.data_);
+    }
 
-    void PushBack(int value);
+    void PushBack(int value) {
+        data_.push_back(value);
+    }
 
-    void PopBack();
+    void PopBack() {
+        data_.pop_back();
+    }
 
-    void PushFront(int value);
+    void PushFront(int value) {
+        data_.push_front(value);
+    }
 
-    void PopFront();
+    void PopFront() {
+        data_.pop_front();
+    }
 
-    int& operator[](size_t ind);
+    int& operator[](size_t ind) {
+        return data_[ind];
+    }
 
-    int operator[](size_t ind) const;
+    int operator[](size_t ind) const {
+        return data_[ind];
+    }
 
-    size_t Size() const;
+    size_t Size() const {
+        return data_.size();
+    }
 
-    void Clear();
-
-    ~Deque();
+    void Clear() {
+        data_.clear();
+    }
 
 private:
-    size_t size_ = 0;
-    size_t capacity_ = 0;
-    size_t amount_of_buffers_ = 0;
-    std::pair<size_t, size_t> front_;
-    std::pair<size_t, size_t> back_;
-    Buffer* data_ = nullptr;
-
-    void Reallocate();
+    std::deque<int> data_;
 };
-
