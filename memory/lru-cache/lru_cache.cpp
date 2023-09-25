2c21b872f8e5a712e8346023d8003304b1aec5b2
Mark Bondarenko <kingsman2gold@gmail.com>
2023-09-25 12:50:59 +0200

    memory/lru-cache

---
+++ memory/lru-cache/lru_cache.cpp
@@ -0,0 +1,60 @@
+#include "lru_cache.h"
+
+LruCache::LruCache(size_t max_size) : capacity_(max_size), size_(0), start_(new Node("", "")), finish_(new Node("", "")) {
+    start_->next = finish_;
+    finish_->prev = start_;
+}
+void LruCache::Set(const std::string& key, const std::string& value) {
+    if (m_.count(key)) {
+        Node* x = m_[key];
+        x->prev->next = x->next;
+        x->next->prev = x->prev;
+        x->next = finish_;
+        x->prev = finish_->prev;
+        x->prev->next = x;
+        finish_->prev = x;
+        x->val = value;
+        return;
+    }
+    if (size_ < capacity_) {
+        Node* t = new Node(value, key);
+        t->next = finish_;
+        t->prev = finish_->prev;
+        t->prev->next = t;
+        finish_->prev = t;
+        ++size_;
+        m_[key] = t;
+        return;
+    }
+    Node* x = start_->next;
+    x->prev->next = x->next;
+    x->next->prev = x->prev;
+    x->next = finish_;
+    x->prev = finish_->prev;
+    x->prev->next = x;
+    finish_->prev = x;
+    m_.erase(x->key);
+    x->key = key;
+    x->val = value;
+    m_[key] = x;
+}
+
+bool LruCache::Get(const std::string& key, std::string* value) {
+    if (m_.count(key)) {
+        Node* x = m_[key];
+        x->prev->next = x->next;
+        x->next->prev = x->prev;
+        x->next = finish_;
+        x->prev = finish_->prev;
+        x->prev->next = x;
+        finish_->prev = x;
+        *value = x->val;
+        return true;
+    }
+    return false;
+}
+
+LruCache::Node::Node(std::string  val, std::string  key) : val(std::move(val)),
+                                                           key(std::move(key)),
+                                                           next(nullptr), prev(nullptr) {}
+
---
+++ memory/lru-cache/lru_cache.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <string>
+#include <unordered_map>
+
+class LruCache {
+public:
+    LruCache(size_t max_size);
+
+    void Set(const std::string& key, const std::string& value);
+
+    bool Get(const std::string& key, std::string* value);
+
+private:
+    struct Node {
+        std::string val;
+        std::string key;
+        Node* next;
+        Node* prev;
+        Node(std::string  val, std::string  key);
+    };
+
+    size_t capacity_;
+    size_t size_;
+    std::unordered_map<std::string, Node*> m_;
+    Node* start_;
+    Node* finish_;
+};
