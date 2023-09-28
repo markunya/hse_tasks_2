797f0e061ad209719863b3f13c11ccca57b5f59a
Mark Bondarenko <kingsman2gold@gmail.com>
2023-09-28 12:25:50 +0200

    move/cow-vector

--- move/cow-vector/cow_vector.cpp
+++ move/cow-vector/cow_vector.cpp
@@ -1,3 +1,83 @@
 #include "cow_vector.h"
 
+COWVector::COWVector()  : data_(new Data()) {}
 
+COWVector::~COWVector() {
+    --data_->counter_of_refs;
+    if (data_->counter_of_refs == 0) {
+        delete data_;
+    }
+}
+
+COWVector::COWVector(const COWVector& other) : data_(other.data_) {
+    ++data_->counter_of_refs;
+}
+
+COWVector& COWVector::operator=(const COWVector& other) {
+    if (this == &other) {
+        return *this;
+    }
+    --data_->counter_of_refs;
+    if (data_->counter_of_refs == 0) {
+        delete data_;
+    }
+    data_ = other.data_;
+    ++data_->counter_of_refs;
+    return *this;
+}
+
+COWVector::COWVector(COWVector&& other) {
+    data_ = other.data_;
+    ++data_->counter_of_refs;
+}
+
+COWVector& COWVector::operator=(COWVector&& other) {
+    if (this == &other) {
+        return *this;
+    }
+    --data_->counter_of_refs;
+    if (data_->counter_of_refs == 0) {
+        delete data_;
+    }
+    data_ = other.data_;
+    ++data_->counter_of_refs;
+    return *this;
+}
+
+size_t COWVector::Size() const {
+    return data_->buffer.size();
+}
+
+void COWVector::Resize(size_t size) {
+    Reallocate();
+    data_->buffer.resize(size);
+}
+
+const std::string& COWVector::Get(size_t at) {
+    return data_->buffer[at];
+}
+
+const std::string& COWVector::Back() {
+    return data_->buffer.back();
+}
+
+void COWVector::PushBack(const std::string& value) {
+    Reallocate();
+    data_->buffer.push_back(value);
+}
+
+void COWVector::Set(size_t at, const std::string& value) {
+    Reallocate();
+    data_->buffer[at] = value;
+}
+
+void COWVector::Reallocate() {
+    if (data_->counter_of_refs > 1) {
+        --data_->counter_of_refs;
+        Data* new_data = new Data();
+        new_data->buffer = data_->buffer;
+        data_ = new_data;
+    }
+}
+
+COWVector::Data::Data() : counter_of_refs(1) {}
--- move/cow-vector/cow_vector.h
+++ move/cow-vector/cow_vector.h
@@ -6,88 +6,38 @@
 
 class COWVector {
 public:
-    COWVector() : data_(new Data()) {}
-    ~COWVector() {
-        --data_->counter_of_refs;
-        if (data_->counter_of_refs == 0) {
-            delete data_;
-        }
-    }
+    COWVector();
+    ~COWVector();
 
-    COWVector(const COWVector& other) : data_(other.data_) {
-        ++data_->counter_of_refs;
-    }
+    COWVector(const COWVector& other);
 
-    COWVector& operator=(const COWVector& other) {
-        if (this == &other) {
-            return *this;
-        }
-        --data_->counter_of_refs;
-        if (data_->counter_of_refs == 0) {
-            delete data_;
-        }
-        data_ = other.data_;
-        ++data_->counter_of_refs;
-        return *this;
-    }
+    COWVector& operator=(const COWVector& other);
 
-    COWVector(COWVector&& other) {
-        std::swap(data_, other.data_);
-    }
+    COWVector(COWVector&& other);
 
-    COWVector& operator=(COWVector&& other) {
-        if (this == &other) {
-            return *this;
-        }
-        --data_->counter_of_refs;
-        if (data_->counter_of_refs == 0) {
-            delete data_;
-        }
-        std::swap(data_, other.data_);
-        return *this;
-    }
+    COWVector& operator=(COWVector&& other);
 
-    size_t Size() const {
-        return data_->buffer.size();
-    }
+    size_t Size() const;
 
-    void Resize(size_t size) {
-        Reallocate();
-        data_->buffer.resize(size);
-    }
+    void Resize(size_t size);
 
-    const std::string& Get(size_t at) {
-        return data_->buffer[at];
-    }
-    const std::string& Back() {
-        return data_->buffer.back();
-    }
+    const std::string& Get(size_t at);
 
-    void PushBack(const std::string& value) {
-        Reallocate();
-        data_->buffer.push_back(value);
-    }
+    const std::string& Back();
 
-    void Set(size_t at, const std::string& value) {
-        Reallocate();
-        data_->buffer[at] = value;
-    }
+    void PushBack(const std::string& value);
+
+    void Set(size_t at, const std::string& value);
 
 private:
     struct Data {
         std::vector<std::string> buffer;
         size_t counter_of_refs;
 
-        Data() : counter_of_refs(1) {}
+        Data();
     };
 
     Data* data_ = nullptr;
 
-    void Reallocate() {
-        if (data_->counter_of_refs > 1) {
-            Data* new_data = new Data();
-            new_data->buffer = data_->buffer;
-            std::swap(data_, new_data);
-        }
-    }
+    void Reallocate();
 };
