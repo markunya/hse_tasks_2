7205d5ce8a32fb3da7485c1ac3e994edb9dedf5e
Mark Bondarenko <kingsman2gold@gmail.com>
2023-11-02 16:23:26 +0100

    meta/compile-eval

--- meta/compile-eval/determinant.h
+++ meta/compile-eval/determinant.h
@@ -5,22 +5,25 @@
 template <int N>
 constexpr int determinant(const int a[N][N]) {
     int det = 0;
-    if (N == 1) {
+    if constexpr (N == 0) {
+        return 1;
+    } else if constexpr (N == 1) {
         return a[0][0];
-    }
-    for (int i = 0; i < N; i++) {
-        int submatrix[N - 1][N - 1];
-        for (int j = 1; j < N; j++) {
-            int k = 0;
-            for (int l = 0; l < N; l++) {
-                if (l != i) {
-                    submatrix[j - 1][k] = a[j][l];
-                    k++;
+    } else {
+        for (int i = 0; i < N; i++) {
+            int submatrix[N - 1][N - 1];
+            for (int j = 1; j < N; j++) {
+                int k = 0;
+                for (int l = 0; l < N; l++) {
+                    if (l != i) {
+                        submatrix[j - 1][k] = a[j][l];
+                        k++;
+                    }
                 }
             }
+            int sign = (i % 2 == 0) ? 1 : -1;
+            det += sign * a[0][i] * determinant<N - 1>(submatrix);
         }
-        int sign = (i % 2 == 0) ? 1 : -1;
-        det += sign * a[0][i] * determinant<N - 1>(submatrix);
     }
     return det;
 }
@@ -34,9 +37,4 @@ constexpr int determinant(const std::array<std::array<int, N>, N>& a) {
         }
     }
     return determinant<N>(matrix);
-}
-
-template <>
-constexpr int determinant<0>(const std::array<std::array<int, 0>, 0>& a) {
-    return 1;
 }
