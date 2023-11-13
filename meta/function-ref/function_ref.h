00c1441f2c052708baddbb70f68b3d591711a6bf
Mark Bondarenko <kingsman2gold@gmail.com>
2023-11-13 16:26:08 +0100

    meta/function-ref

---
+++ meta/function-ref/function_ref.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include <utility>
+#include <functional>
+
+template <typename Signature>
+class FunctionRef;
+
+template <typename R, typename... Args>
+class FunctionRef<R(Args...)> {
+public:
+    template <typename F>
+    FunctionRef(F&& f) {
+        Builder(f);
+    }
+
+    template <typename F>
+    FunctionRef& operator=(F&& f) {
+        Builder(f);
+    }
+
+    R operator()(Args... args) const {
+        return static_cast<R>(springboard_(callback_, std::forward<Args>(args)...));
+    }
+
+private:
+    void* callback_ = nullptr;
+    R (*springboard_)(void*, Args...) = nullptr;
+
+    template <class F>
+    void Builder(F& f) {
+        callback_ = reinterpret_cast<void*>(std::addressof(f));
+        springboard_ = [](void* callback, Args... args) {
+            return static_cast<R>(std::invoke(
+                *reinterpret_cast<typename std::add_pointer_t<F>>(callback),
+                std::forward<Args>(args)...));
+        };
+    }
+
+    template <typename R_, typename... Args_>
+    void Builder(R_ (*f)(Args_...)) {
+        callback_ = reinterpret_cast<void*>(f);
+        springboard_ = [](void* callback, Args... args) {
+            return static_cast<R>(
+                std::invoke(reinterpret_cast<decltype(f)>(callback), std::forward<Args>(args)...));
+        };
+    }
+};
