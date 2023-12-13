6fd7397a8bae248abc1177fcfea8cff977bd4096
Mark Bondarenko <kingsman2gold@gmail.com>
2023-12-13 22:10:02 +0100

    lock-free/futex

---
+++ lock-free/futex/mutex.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include <linux/futex.h>
+#include <sys/syscall.h>
+#include <sys/time.h>
+#include <unistd.h>
+
+#include <mutex>
+#include <atomic>
+
+// Atomically do the following:
+//    if (*value == expected_value) {
+//        sleep_on_address(value)
+//    }
+void FutexWait(int *value, int expected_value) {
+    syscall(SYS_futex, value, FUTEX_WAIT_PRIVATE, expected_value, nullptr, nullptr, 0);
+}
+
+// Wakeup 'count' threads sleeping on address of value(-1 wakes all)
+void FutexWake(int *value, int count) {
+    syscall(SYS_futex, value, FUTEX_WAKE_PRIVATE, count, nullptr, nullptr, 0);
+}
+
+class Mutex {
+public:
+    void Lock() {
+        int t = 0;
+        if (!a_.compare_exchange_strong(t, 1)) {
+            do {
+                if (t == 2 || a_.compare_exchange_strong(t, 2)) {
+                    FutexWait(reinterpret_cast<int *>(&a_), 2);
+                }
+            } while (a_.compare_exchange_strong(t, 2));
+        }
+    }
+
+    void Unlock() {
+        if (a_.fetch_sub(1) != 1) {
+            a_.store(0);
+            FutexWake(reinterpret_cast<int *>(&a_), 1);
+        }
+    }
+
+private:
+    std::atomic<int> a_ = 0;
+};
