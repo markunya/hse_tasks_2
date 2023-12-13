73cde66d7eed1ee13f5bdc8b53d8b86526f20017
Mark Bondarenko <kingsman2gold@gmail.com>
2023-12-13 22:12:17 +0100

    lock-free/futex

--- lock-free/futex/mutex.h
+++ lock-free/futex/mutex.h
@@ -4,19 +4,12 @@
 #include <sys/syscall.h>
 #include <sys/time.h>
 #include <unistd.h>
-
-#include <mutex>
 #include <atomic>
 
-// Atomically do the following:
-//    if (*value == expected_value) {
-//        sleep_on_address(value)
-//    }
 void FutexWait(int *value, int expected_value) {
     syscall(SYS_futex, value, FUTEX_WAIT_PRIVATE, expected_value, nullptr, nullptr, 0);
 }
 
-// Wakeup 'count' threads sleeping on address of value(-1 wakes all)
 void FutexWake(int *value, int count) {
     syscall(SYS_futex, value, FUTEX_WAKE_PRIVATE, count, nullptr, nullptr, 0);
 }
