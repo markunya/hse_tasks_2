c8383a610de2adab22beceb57459b63d1cb18cec
Mark Bondarenko <kingsman2gold@gmail.com>
2024-01-13 18:09:22 +0100

    bonus/executors

---
+++ bonus/executors/executors.cpp
@@ -0,0 +1,219 @@
+#include <executors.h>
+
+Task::Task() {
+    static std::atomic<size_t> prev_id{1};
+    id_ = prev_id.load();
+    while (!prev_id.compare_exchange_weak(id_, id_ + 1)) {
+    }
+}
+
+void Task::AddDependency(std::shared_ptr<Task> task) {
+    task->dependent_on_me_.emplace_back(shared_from_this());
+    need_dependencies_.fetch_add(1);
+    can_be_done_.store(false);
+}
+
+void Task::AddTrigger(std::shared_ptr<Task> task) {
+    task->triggered_by_me_.emplace_back(shared_from_this());
+    can_be_done_.store(false);
+}
+
+void Task::SetTimeTrigger(std::chrono::system_clock::time_point at) {
+    deadline_ = at;
+    can_be_done_.store(false);
+}
+
+bool Task::IsCompleted() {
+    return is_completed_.load();
+}
+
+bool Task::IsFailed() {
+    return is_failed_.load();
+}
+
+bool Task::IsCanceled() {
+    return is_canceled_.load();
+}
+
+bool Task::IsFinished() {
+    return IsFailed() || IsCanceled() || IsCompleted();
+}
+
+std::exception_ptr Task::GetError() {
+    return error_;
+}
+
+void Task::Cancel() {
+    auto guard1 = std::unique_lock{task_mutex_};
+    auto guard2 = std::unique_lock{global};
+    bool t = false;
+    if (is_canceled_.compare_exchange_strong(t, true)) {
+        for (auto task : dependent_on_me_) {
+            task->need_dependencies_.fetch_sub(1);
+            if (task->need_dependencies_.load() == 0) {
+                task->can_be_done_.store(true);
+            }
+        }
+        for (auto task : triggered_by_me_) {
+            task->can_be_done_.store(true);
+        }
+        have_job.notify_one();
+        done_.notify_one();
+    }
+}
+
+void Task::Wait() {
+    auto guard = std::unique_lock{task_mutex_};
+    while (!IsFinished()) {
+        done_.wait(guard);
+    }
+}
+
+bool Task::SubNeedDependencies() {
+    size_t prev = need_dependencies_.load();
+    do {
+        if (prev == 0) {
+            throw std::runtime_error("Sub when zero dependencies are needed");
+        }
+    } while (!need_dependencies_.compare_exchange_weak(prev, prev - 1));
+    if (need_dependencies_.load() == 0) {
+        return true;
+    }
+    return false;
+}
+
+void Task::SetWhenFinished(std::chrono::system_clock::time_point at) {
+}
+
+Task::~Task() = default;
+
+Executor::Executor(size_t num_of_threads) {
+    workers_.reserve(num_of_threads);
+    for (size_t i = 0; i < num_of_threads; ++i) {
+        workers_.emplace_back(&Executor::TakeTask, this);
+    }
+}
+
+void Executor::Submit(std::shared_ptr<Task> task) {
+    auto guard = std::unique_lock{Task::global};
+    if (task->can_be_done_.load() ||
+        (task->deadline_ < std::chrono::system_clock::now() &&
+         task->deadline_ != std::numeric_limits<std::chrono::system_clock::time_point>::min())) {
+        ready_.push(task);
+        Task::have_job.notify_one();
+        return;
+    }
+    if (task->deadline_ != std::numeric_limits<std::chrono::system_clock::time_point>::min()) {
+        timer_heap_.push(task);
+        Task::have_job.notify_one();
+    }
+    tasks_.emplace_back(task);
+}
+
+void Executor::StartShutdown() {
+    is_shut_downed_.store(true);
+    auto guard = std::unique_lock{Task::global};
+    Task::have_job.notify_all();
+}
+
+void Executor::WaitShutdown() {
+    auto guard = std::unique_lock{Task::global};
+    while (shut_downed_threads_ < workers_.size()) {
+        shutdown_complete_.wait(guard);
+    }
+}
+
+void Executor::TakeTask() {
+    auto guard = std::unique_lock{Task::global};
+    while (!is_shut_downed_.load()) {
+        std::shared_ptr<Task> task;
+        while (!timer_heap_.empty() &&
+               (timer_heap_.top()->deadline_ < std::chrono::system_clock::now() ||
+                timer_heap_.top()->can_be_done_.load())) {
+            timer_heap_.top()->can_be_done_.store(true);
+            timer_heap_.pop();
+        }
+        if (ready_.empty()) {
+            auto it = tasks_.begin();
+            while (it != tasks_.end()) {
+                auto current = it++;
+                if ((*current)->can_be_done_) {
+                    auto task_mb_next = (*current);
+                    tasks_.erase(current);
+                    ready_.push(task_mb_next);
+                    Task::have_job.notify_one();
+                }
+            }
+        }
+        if (!ready_.empty()) {
+            task = ready_.front();
+            ready_.pop();
+        }
+        if (!task) {
+            if (!timer_heap_.empty()) {
+                Task::have_job.wait_until(guard, timer_heap_.top()->deadline_);
+            } else {
+                Task::have_job.wait_for(guard, std::chrono::milliseconds(500));
+            }
+            continue;
+        }
+        guard.unlock();
+        DoTask(task);
+        guard.lock();
+    }
+    shut_downed_threads_.fetch_add(1);
+    if (shut_downed_threads_ == workers_.size()) {
+        shutdown_complete_.notify_all();
+    }
+}
+
+void Executor::DoTask(std::shared_ptr<Task> task) {
+    if (task->is_canceled_.load()) {
+        NotifyTask(task);
+        return;
+    }
+    try {
+        task->Run();
+    } catch (...) {
+        task->error_ = std::current_exception();
+        task->is_failed_.store(true);
+        NotifyTask(task);
+        return;
+    }
+    task->is_completed_.store(true);
+    NotifyTask(task);
+}
+
+void Executor::NotifyTask(std::shared_ptr<Task> task) {
+    auto guard = std::unique_lock{Task::global};
+    for (auto i : task->triggered_by_me_) {
+        i->can_be_done_.store(true);
+        i->triggered_by_.store(task->id_);
+        Task::have_job.notify_one();
+    }
+    for (auto i : task->dependent_on_me_) {
+        if (i->can_be_done_.load()) {
+            continue;
+        }
+        if (i->SubNeedDependencies()) {
+            i->can_be_done_.store(true);
+            Task::have_job.notify_one();
+        }
+    }
+    task->SetWhenFinished(std::chrono::system_clock::now());
+    {
+        auto task_guard = std::unique_lock{task->task_mutex_};
+        task->done_.notify_all();
+    }
+}
+
+Executor::~Executor() {
+    StartShutdown();
+    for (auto& worker : workers_) {
+        worker.join();
+    }
+}
+
+std::shared_ptr<Executor> MakeThreadPoolExecutor(int num_threads) {
+    return std::make_shared<Executor>(num_threads);
+}
---
+++ bonus/executors/executors.h
@@ -0,0 +1,234 @@
+#include <memory>
+#include <chrono>
+#include <vector>
+#include <functional>
+#include <atomic>
+#include <mutex>
+#include <condition_variable>
+#include <thread>
+#include <queue>
+#include <list>
+#include <optional>
+
+class Task : public std::enable_shared_from_this<Task> {
+public:
+    Task();
+
+    virtual void Run() = 0;
+
+    // Task::run() completed without throwing exception
+    bool IsCompleted();
+
+    // Task::run() throwed exception
+    bool IsFailed();
+
+    // Task was Canceled
+    bool IsCanceled();
+
+    // Task either completed, failed or was Canceled
+    bool IsFinished();
+
+    std::exception_ptr GetError();
+
+    void Wait();
+
+    void Cancel();
+
+    void AddDependency(std::shared_ptr<Task> task);
+
+    void AddTrigger(std::shared_ptr<Task> task);
+
+    void SetTimeTrigger(std::chrono::system_clock::time_point at);
+
+    virtual ~Task();
+
+private:
+    friend class Executor;
+
+    bool SubNeedDependencies();
+
+    virtual void SetWhenFinished(std::chrono::system_clock::time_point at);
+
+    std::mutex task_mutex_;
+    std::condition_variable done_;
+    inline static std::mutex global;
+    inline static std::condition_variable have_job;
+
+    size_t id_ = 0;
+    std::atomic<size_t> triggered_by_{0};
+    std::atomic<size_t> need_dependencies_ = 0;
+    std::atomic<bool> can_be_done_{true};
+    std::atomic<bool> is_completed_{false};
+    std::atomic<bool> is_failed_{false};
+    std::atomic<bool> is_canceled_{false};
+    std::vector<std::shared_ptr<Task>> dependent_on_me_;
+    std::vector<std::shared_ptr<Task>> triggered_by_me_;
+    std::exception_ptr error_ = nullptr;
+    std::chrono::system_clock::time_point deadline_ =
+        std::numeric_limits<std::chrono::system_clock::time_point>::min();
+};
+
+template <class T>
+class Future;
+
+template <class T>
+using FuturePtr = std::shared_ptr<Future<T>>;
+
+// Used instead of void in generic code
+struct Unit {};
+
+class Executor {
+public:
+    Executor(size_t num_of_threads = std::thread::hardware_concurrency());
+
+    void Submit(std::shared_ptr<Task> task);
+
+    void StartShutdown();
+    void WaitShutdown();
+
+    template <class T>
+    FuturePtr<T> Invoke(std::function<T()> fn) {
+        auto task = std::make_shared<Future<T>>();
+        task->Set(fn);
+        Submit(task);
+        return std::shared_ptr<Future<T>>(task);
+    }
+
+    template <class Y, class T>
+    FuturePtr<Y> Then(FuturePtr<T> input, std::function<Y()> fn) {
+        auto task = std::make_shared<Future<Y>>();
+        task->Set(fn);
+        task->AddDependency(input);
+        Submit(task);
+        return task;
+    }
+
+    template <class T>
+    FuturePtr<std::vector<T>> WhenAll(std::vector<FuturePtr<T>> all) {
+        auto task = std::make_shared<Future<std::vector<T>>>();
+        for (auto dep : all) {
+            task->AddDependency(dep);
+        }
+        task->Set([all]() -> std::vector<T> {
+            std::vector<T> res;
+            res.reserve(all.size());
+            for (auto f : all) {
+                res.emplace_back(f->Get());
+            }
+            return res;
+        });
+        Submit(task);
+        return task;
+    }
+
+    template <class T>
+    FuturePtr<T> WhenFirst(std::vector<FuturePtr<T>> all) {
+        auto task = std::make_shared<Future<T>>();
+        for (auto tr : all) {
+            task->AddTrigger(tr);
+        }
+        task->Set([all, task]() {
+            for (auto tr : all) {
+                if (tr->id_ == task->triggered_by_) {
+                    return tr->Get();
+                }
+            }
+        });
+        Submit(task);
+        return task;
+    }
+
+    template <class T>
+    FuturePtr<std::vector<T>> WhenAllBeforeDeadline(
+        std::vector<FuturePtr<T>> all, std::chrono::system_clock::time_point deadline) {
+        auto task = std::make_shared<Future<std::vector<T>>>();
+        task->SetTimeTrigger(deadline);
+        task->Set([all, deadline]() {
+            std::vector<T> res;
+            for (auto f : all) {
+                if (f->finished_at_ < deadline) {
+                    res.emplace_back(f->Get());
+                }
+            }
+            return res;
+        });
+        Submit(task);
+        return task;
+    }
+
+    ~Executor();
+
+private:
+    void NotifyTask(std::shared_ptr<Task> task);
+
+    void TakeTask();
+
+    void DoTask(std::shared_ptr<Task> task);
+
+    std::atomic<bool> is_shut_downed_{false};
+    std::atomic<uint8_t> shut_downed_threads_;
+    std::condition_variable shutdown_complete_;
+    std::vector<std::thread> workers_;
+    std::queue<std::shared_ptr<Task>> ready_;
+    std::priority_queue<std::shared_ptr<Task>, std::vector<std::shared_ptr<Task>>,
+                        decltype([](std::shared_ptr<Task> first, std::shared_ptr<Task> second) {
+                            return first->deadline_ > second->deadline_;
+                        })>
+        timer_heap_;
+    std::list<std::shared_ptr<Task>> tasks_;
+};
+
+std::shared_ptr<Executor> MakeThreadPoolExecutor(int num_threads);
+
+template <class T>
+class Future : public Task {
+public:
+    T Get();
+
+private:
+    friend class Executor;
+
+    void Set(std::function<T()> fn);
+
+    void Run() override;
+
+    void SetWhenFinished(std::chrono::system_clock::time_point at) override;
+
+    std::mutex m_;
+    std::function<T()> fn_;
+
+    std::condition_variable data_loaded_;
+    std::chrono::system_clock::time_point finished_at_ =
+        std::chrono::time_point<std::chrono::system_clock>::max();
+    std::optional<T> data_ = std::nullopt;
+    std::exception_ptr error_ = nullptr;
+};
+
+template <class T>
+T Future<T>::Get() {
+    Wait();
+    if (error_) {
+        std::rethrow_exception(error_);
+    }
+    return data_.value();
+}
+
+template <typename T>
+void Future<T>::Set(std::function<T()> fn) {
+    fn_ = std::move(fn);
+}
+
+template <typename T>
+void Future<T>::Run() {
+    try {
+        data_ = std::invoke(fn_);
+    } catch (...) {
+        error_ = std::current_exception();
+    }
+    data_loaded_.notify_all();
+}
+
+template <class T>
+void Future<T>::SetWhenFinished(std::chrono::system_clock::time_point at) {
+    finished_at_ = at;
+}
